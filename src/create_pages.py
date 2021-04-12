41240beb4ae821e9c19a664292def3e986f2e73f
renevinaya <65111407+renevinaya@users.noreply.github.com>
2021-04-12 21:15:34 +0200

    Improve layout

--- src/create_pages.py
+++ src/create_pages.py
@@ -15,7 +15,7 @@ from typing import Union
 
 # Settings
 URL_PLECO_FLASHCARD_DATABASE: str = 'http://raspi4/sync/phone/pleco/databases/Pleco%20Flashcard%20Database.pqb'
-FONT_CHINESE: ImageFont = ImageFont.truetype('/usr/share/fonts/opentype/noto/NotoSansCJK-Bold.ttc', 140)
+FONT_CHINESE: ImageFont = ImageFont.truetype('/usr/share/fonts/opentype/noto/NotoSerifCJK-Bold.ttc', 140)
 FONT_LATIN: ImageFont = ImageFont.truetype('/usr/share/fonts/truetype/noto/NotoSans-Regular.ttf', 30)
 HEIGHT: int = 384
 WIDTH: int = 640
@@ -63,8 +63,8 @@ def create_page(day_of_month, defn, hw, pron):
     # Draw image for each day
     image: Image = Image.new("1", SIZE, color=1)
     draw: ImageDraw = ImageDraw.Draw(image)
-    draw_centered_text(CENTER_HEIGHT - 80, hw, FONT_CHINESE, draw)
-    draw_centered_text(CENTER_HEIGHT + 60, pron, FONT_LATIN, draw)
+    draw_centered_text(CENTER_HEIGHT - 20, hw, FONT_CHINESE, draw)
+    draw_centered_text(CENTER_HEIGHT - 120, pron, FONT_LATIN, draw)
     draw_centered_text(CENTER_HEIGHT + 120, defn, FONT_LATIN, draw)
     image.save("out/{0:1d}.gif".format(day_of_month))
     image.close()
