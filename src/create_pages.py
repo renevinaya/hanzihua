92d499fadc744efb519e6c8658d9246491bd74d9
renevinaya <65111407+renevinaya@users.noreply.github.com>
2021-04-14 21:51:48 +0200

    Use CE_CCDICT for words linked to proprietary pleco database

--- src/create_pages.py
+++ src/create_pages.py
@@ -7,14 +7,16 @@ import requests
 import sqlite3
 import tempfile
 import os
+import gzip
 from dragonmapper import transcriptions
 from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
-from typing import Union
+from typing import Union, Dict, Tuple
 
 # Settings
 URL_PLECO_FLASHCARD_DATABASE: str = 'http://raspi4/sync/phone/pleco/databases/Pleco%20Flashcard%20Database.pqb'
+URL_CC_CEDICT_DATABASE: str = 'https://www.mdbg.net/chinese/export/cedict/cedict_1_0_ts_utf-8_mdbg.txt.gz'
 FONT_CHINESE: ImageFont = ImageFont.truetype('/usr/share/fonts/opentype/noto/NotoSerifCJK-Bold.ttc', 200)
 FONT_LATIN: ImageFont = ImageFont.truetype('/usr/share/fonts/truetype/noto/NotoSans-Regular.ttf', 40)
 HEIGHT: int = 384
@@ -27,16 +29,15 @@ CENTER_WIDTH: int = int(WIDTH / 2)
 
 def main():
     # Download Database
-    _, database_file = tempfile.mkstemp(prefix='hanzihua-')
-    with open(database_file, "wb") as f:
+    _, pleco_database_file = tempfile.mkstemp(prefix='hanzihua-')
+    with open(pleco_database_file, "wb") as f:
         with requests.get(URL_PLECO_FLASHCARD_DATABASE) as r:
             f.write(r.content)
-    with sqlite3.connect(database_file) as database:
+    with sqlite3.connect(pleco_database_file) as database:
         # Get 31 flash cards sorted by score hw=Chinese pron=Pronunciation, defn=Definition
-        # Use flash cards with hard coded definition only
         cards = database.cursor().execute(
             'select c.hw, c.pron, c.defn from pleco_flash_scores_1 s, pleco_flash_cards c ' +
-            'where c.id = s.card and c.defn NOT NULL order by s.score limit 60')
+            'where c.id = s.card order by s.score limit 60')
 
         day_of_month: int = 1
         for hw, pron, defn in cards:
@@ -46,7 +47,7 @@ def main():
             if day_of_month == 32:
                 break
     # Tidy up
-    os.unlink(database_file)
+    os.unlink(pleco_database_file)
 
 
 def create_page(day_of_month, defn, hw, pron) -> bool:
@@ -54,17 +55,27 @@ def create_page(day_of_month, defn, hw, pron) -> bool:
     hw: str = hw.replace('@', '')
     if len(hw) > 3:
         # Skip long words
+        print('Too long Chinese:', hw)
         return False
     pron: str = pron.replace('@', '')
     pron: str = transcriptions.numbered_to_accented(pron)
-    defn: str = defn.split('\n')[0]
-    defn: str = defn.replace('• ', '')
-    defn: str = defn.split('  ')[0]
-    defn: str = defn.split(',')[0]
-    defn: str = defn.split(';')[0]
-    defn: str = defn.split('/')[0]
-    defn: str = defn.strip()
-    if len(defn) > 30:
+    if defn is None:
+        if hw in CE_CCDICT:
+            defn = CE_CCDICT[hw]
+        else:
+            print('No Translation:', hw, pron)
+            return False
+    else:
+        defn: str = defn.split('\n')[0]
+        defn: str = defn.replace('• ', '')
+        defn: str = defn.split('  ')[0]
+        defn: str = defn.split(',')[0]
+        defn: str = defn.split(';')[0]
+        defn: str = defn.split('/')[0]
+        defn: str = defn.strip()
+    width_defn, _ = FONT_LATIN.getsize(defn)
+    if width_defn > WIDTH:
+        print('Too long translation', hw, pron, defn)
         return False
 
     # Draw image for each day
@@ -84,5 +95,32 @@ def draw_centered_text(y: Union[int, float], text: str, font: ImageFont, draw: I
     y_position: int = int(y - (height_of_text / 2))
     draw.text((x_position, y_position), text, fill=0, font=font)
 
+
+def get_ce_ccdict() -> Dict[str, str]:
+    ce_ccdict: Dict[str, str] = {}
+    with requests.get(URL_CC_CEDICT_DATABASE) as r:
+        ce_ccdict_bytes: bytes = gzip.decompress(r.content)
+        for entry_bytes in ce_ccdict_bytes.split(b'\n'):
+            line: str = entry_bytes.decode()
+            if line.startswith('#'):
+                continue
+            # Credits:
+            # https://github.com/rubber-duck-dragon/rubber-duck-dragon.github.io/blob/master/cc-cedict_parser/parser.py
+            line = line.rstrip('/')
+            line = line.split('/')
+            if len(line) <= 1:
+                continue
+            english = line[1]
+            char_and_pinyin = line[0].split('[')
+            characters = char_and_pinyin[0]
+            characters = characters.split()
+            simplified = characters[1]
+            if "surname" != english:
+                ce_ccdict[simplified] = english
+
+    return ce_ccdict
+
+
+CE_CCDICT: Dict[str, str] = get_ce_ccdict()
 
 main()
