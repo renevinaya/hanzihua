da410cb2651757006a354f70dcf06b4a95ffaf82
René Mueller <rene.mueller@posteo.de>
2025-01-05 22:23:50 +0100

    make independent from working directory

--- src/display.py
+++ src/display.py
@@ -5,10 +5,12 @@ Link it in /etc/cron.daily to run it daily. Must be run as root to access ePaper
 """
 
 import datetime
+import pathlib
 from PIL import Image
 from waveshare_epd import epd7in5
 
-image = Image.open(f"out/{datetime.datetime.today().day:1d}.gif")
+folder = pathlib.Path(__file__).parent.resolve()
+image = Image.open(f"{folder}/out/{datetime.datetime.today().day:1d}.gif")
 
 epd = epd7in5.EPD()
 epd.init()
